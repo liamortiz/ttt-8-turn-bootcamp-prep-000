91b1e11c6f771c4bbe4455915b8455361c174464
Leamsi Escribano <leamsii.e@gmail.com>
2020-05-09 21:03:37 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,18 @@
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def valid_move?(board, index)
+  board[index] ? board[index].strip == "" : false
+end
+
+def move(board, index, token='X')
+end
+
+def input_to_index(index)
+  index.to_i - 1
+end
