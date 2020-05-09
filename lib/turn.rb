67d3ded378b94fdee383f64b6848449ee9c23a14
Leamsi Escribano <leamsii.e@gmail.com>
2020-05-09 21:20:50 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -22,5 +22,13 @@ end
 
 def turn(board)
   puts "Please enter 1-9:"
+  
   user_input = input_to_index(gets.strip)
+  
+  if user_input == -1
+    turn(board)
+  end
+  
+  move(board, user_input)
+  display_board(board)
 end
